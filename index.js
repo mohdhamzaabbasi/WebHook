6f242aa24cb0771f910f322a44eb9657f0c92ba5
Mohd Hamza Abbasi <141723368+mohdhamzaabbasi@users.noreply.github.com>
2025-04-02 01:53:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => console.log('Webhook server running on port 3000'));
+app.listen(3000, () => console.log('Webhook server running on port 5000'));
 
 app.get('/', async (req, res) => {
     try {
